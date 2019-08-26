db7dfa6049f2b8b7529f64efc3cad88962014a9d
Edison Panchi <edison.panchi@gmail.com>
2019-08-26 16:02:26 -0500

    masive create contact

---
+++ create-contact.php
@@ -0,0 +1,37 @@
+<?php
+
+include('vendor/autoload.php');
+$dotenv = Dotenv\Dotenv::create(__DIR__, '.env');
+$dotenv->load();
+
+$ac = new ActiveCampaign(getenv('ACTIVECAMPAIGN_URL'), getenv('ACTIVECAMPAIGN_KEY'));
+
+// VALIDATE ACCESS
+
+if (!(int) $ac->credentials_test()) {
+    echo "<p>Access denied: Invalid credentials (URL and/or API key).</p>";
+    exit();
+}
+
+$listId = 1;
+
+$faker = Faker\Factory::create();
+
+for ($i = 0; $i < 5; $i++) {
+    $contact = array(
+        "email"              => $faker->email,
+        "first_name"         => $faker->firstName,
+        "last_name"          => $faker->lastName,
+        "p[{$listId}]"      => $listId,
+        "status[{$listId}]" => 1, // "Active" status
+    );
+    $contact_sync = $ac->api("contact/sync", $contact);
+    if (!(int) $contact_sync->success) {
+        // request failed
+        echo "<p>Syncing contact failed. Error returned: " . $contact_sync->error . "</p>";
+        exit();
+    }
+    // successful request
+    $contact_id = (int) $contact_sync->subscriber_id;
+    echo "<p>Contact synced successfully (ID {$contact_id})!</p>";
+}
--- index.php
+++ index.php
@@ -3,8 +3,50 @@
 include('vendor/autoload.php');
 $dotenv = Dotenv\Dotenv::create(__DIR__, '.env');
 $dotenv->load();
+$ac = new ActiveCampaign(getenv('ACTIVECAMPAIGN_URL'), getenv('ACTIVECAMPAIGN_KEY'));
 
+/*
+	 * TEST API CREDENTIALS.
+	 */
 
+if (!(int) $ac->credentials_test()) {
+    echo "<p>Access denied: Invalid credentials (URL and/or API key).</p>";
+    exit();
+}
 
-$response = file_get_contents(getenv('ACTIVE_CAMPAIGN_URL'));
-var_dump(json_decode($response));
+echo "<p>Credentials valid! Proceeding...</p>";
+
+/*
+	 * VIEW ACCOUNT DETAILS.
+	 */
+
+$account = $ac->api("account/view");
+
+echo "<pre>";
+print_r($account);
+echo "</pre>";
+
+$faker = Faker\Factory::create();
+
+// generate data by accessing properties
+echo $faker->name;
+
+?>
+<script type="text/javascript">
+    (function(e, t, o, n, p, r, i) {
+        e.visitorGlobalObjectAlias = n;
+        e[e.visitorGlobalObjectAlias] = e[e.visitorGlobalObjectAlias] || function() {
+            (e[e.visitorGlobalObjectAlias].q = e[e.visitorGlobalObjectAlias].q || []).push(arguments)
+        };
+        e[e.visitorGlobalObjectAlias].l = (new Date).getTime();
+        r = t.createElement("script");
+        r.src = o;
+        r.async = true;
+        i = t.getElementsByTagName("script")[0];
+        i.parentNode.insertBefore(r, i)
+    })(window, document, "https://diffuser-cdn.app-us1.com/diffuser/diffuser.js", "vgo");
+    vgo('setAccount', '610264226');
+    vgo('setTrackByDefault', true);
+
+    vgo('process');
+</script>

eebd56e45f41fdc3f77314d9597e0a5000681063
Edison Panchi <edison.panchi@gmail.com>
2019-08-26 15:10:20 -0500

    Include ac-libraries

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
 include('vendor/autoload.php');
-$dotenv = Dotenv\Dotenv::create(__DIR__);
-var_dump($dotenv->load());
+$dotenv = Dotenv\Dotenv::create(__DIR__, '.env');
+$dotenv->load();
+
+
+
+$response = file_get_contents(getenv('ACTIVE_CAMPAIGN_URL'));
+var_dump(json_decode($response));
--- readme.md
+++ readme.md
@@ -1,2 +1,5 @@
 GET STARTED
 https://developers.activecampaign.com/page/getting-started
+
+Oficial ACtive Campaign for PHP
+https://github.com/ActiveCampaign/activecampaign-api-php
